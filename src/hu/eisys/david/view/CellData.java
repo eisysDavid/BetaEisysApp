1f9fa4a04172e83fcd375815cb45de68567eefa9
eisysDavid <david.turi@eisys.hu>
2018-08-31 14:14:38 +0200

    Celldata changed

--- src/hu/eisys/david/view/CellData.java
+++ src/hu/eisys/david/view/CellData.java
@@ -4,33 +4,14 @@ import javax.swing.JTable;
 
 public class CellData {
 	
-	private  Object value;
-	private  int col;
 	private  JTable table;
-	private  int row;
 
-	
-	
-	public int getRow() {
-		return row;
-	}
 
 	public CellData(JTable source) {
-		this.col = source.getSelectedColumn();
-		this.row = source.getSelectedRow();
-		this.value = source.getValueAt(row, col);
 		this.table = source;
 	}
 	
 	public CellData() {
-	}
-
-	public int getColumn() {
-		return col;
-	}
-
-	public Object getValue() {
-		return value;
 	}
 
 	public JTable getTable() {
