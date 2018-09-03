eccd43664b94f7b908a8dff2163fdd5d2f4b89d0
eisysDavid <david.turi@eisys.hu>
2018-09-03 08:22:45 +0200

    deleted

--- src/hu/eisys/david/view/IPopup.java
+++
@@ -1,7 +0,0 @@
-package hu.eisys.david.view;
-
-public interface IPopup extends IPopupModify{
-	
-	public void dbChangeEvent(String dbName,String userName,String password);
-	public void addScriptEvent(String scriptName,String myScript);
-}
--- src/hu/eisys/david/view/IUpdate.java
+++
@@ -1,5 +0,0 @@
-package hu.eisys.david.view;
-
-public interface IUpdate {
-	void updateArrayList(int dropRow);
-}
